c7ed1b2bb165fcebc085e31f3d86b82f666c2676
James Maupin <jmsMaupinRC@gmail.com>
2020-03-05 15:24:50 -0500

    When a user is created create a root folder for them as well

--- fileuser/models.py
+++ fileuser/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from file.models import File
+
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    folder = models.OneToOneField(File, on_delete=models.CASCADE, null=True)
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.folder = File.objects.create(
+                name="{}'s folder".format(self.username),
+                parent=None
+            ) 
+            super().save(*args, **kwargs)
